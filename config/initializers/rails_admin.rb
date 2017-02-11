048723820ab32ab6993c311418eed05bf41a96ba
Alex Calgaro <alexjcalgaro@gmail.com>
2017-02-11 16:49:14 -0200

    First commit project Admin

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,30 @@
+RailsAdmin.config do |config|
+  config.main_app_name = Proc.new { |controller| [ "OneBnb - AjC", "Admin - #{controller.params[:action].try(:titleize)}" ] }
+ 
+  ## == Devise ==
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
+ 
+  config.authorize_with do
+    if current_user.kind != "admin"
+      reset_session
+      redirect_to '/users/sign_in'
+    end
+  end
+ 
+  config.excluded_models << "Photo"
+ 
+  config.actions do
+    dashboard                     # mandatory
+    index                         # mandatory
+    new
+    export
+    bulk_delete
+    show
+    edit
+    delete
+    show_in_app
+  end
+end
